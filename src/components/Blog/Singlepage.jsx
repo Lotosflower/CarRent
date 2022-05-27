360f087b41a2a6bd2caea9944d9c6ab6100d6fa9
Yuri <korytko_88@mail.ru>
2022-05-27 18:52:19 +0300

    delete comments

--- src/components/Blog/Singlepage.jsx
+++ src/components/Blog/Singlepage.jsx
@@ -1,35 +1,32 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styles from "./Blog.module.css";
-import {Comments} from "./Comments";
-
-
 
 export const Singlepage = () => {
-const {id} = useParams();
-const [posts, setPosts] = useState(null);
+  const { id } = useParams();
+  const [posts, setPosts] = useState(null);
 
-   useEffect(() => {
+  useEffect(() => {
     fetch(`http://localhost:3001/posts/${id}`)
-                .then(res => res.json())
-                .then(json => setPosts(json))
-    }, [id])
-   return(
-       <>
-        <div className = {styles.blog__container}>
-            <div className = {styles.single__page}>
-                {posts && (
-                    <>
-                    <h1>{posts.title}</h1>
-                    <p>{posts.body}</p>
-                    <p>{posts.body2}</p>
-                    <p>{posts.body3}</p>
-                     
-                    </>  
-                )}
-            </div>
-            <Comments/>
+      .then((res) => res.json())
+      .then((json) => setPosts(json));
+  }, [id]);
+  return (
+    <>
+      <div className={styles.blog__container}>
+        <div className={styles.single__page}>
+          {posts && (
+            <>
+              <h1>{posts.title}</h1>
+              <p>{posts.body}</p>
+              <p>{posts.body2}</p>
+              <p>{posts.body3}</p>
+            </>
+          )}
         </div>
-       </>
-    )
-}
+
+        {/* <Comments/> */}
+      </div>
+    </>
+  );
+};
